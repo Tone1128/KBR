90c23ffc8f89220b3cf3d01457f740a7a76aa8de
Tone1128 <49022967+Tone1128@users.noreply.github.com>
2019-06-05 16:57:01 +0800

    Add files via upload

---
+++ GCVersion.cs
@@ -0,0 +1,23 @@
+//--------------------------------------------------------------------
+//
+//  File: GCVersion.cs
+//
+//  Description: Unicode Uniscribe utilities
+//
+//  Copyright (c) 2001-2008 by FarPoint Technologies, Inc.
+//
+//  All rights reserved.  No part of this source code may be
+//  copied, modified or reproduced in any form without retaining
+//  the above copyright notice.  This source code, or source code
+//  derived from it, may not be redistributed without express
+//  written permission of FarPoint Technologies, Inc.
+//
+//--------------------------------------------------------------------
+
+using System;
+using System.Text;
+static class GCVersionUtil
+{
+  public const string ProductVersion = "9.20.20153.0";
+  public const string ProductFileVersion = "9.20.20153.0";
+}
